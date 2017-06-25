7ac46ad35cf2ba35fcde87fa1bff247aaf2baf23
Penkey Suresh <penkeysuresh@gmail.com>
2017-06-25 23:30:43 +0530

    adding id to admin panel show

--- zruser/admin.py
+++ zruser/admin.py
@@ -5,13 +5,21 @@ from django.contrib import admin
 
 from zruser.models import *
 
+
 # Register your models here.
 
+
+class ZrUserAdmin(admin.ModelAdmin):
+    readonly_fields = ('id', )
+
+    class Meta:
+        model = ZrUser
+
+admin.site.register(ZrUser, ZrUserAdmin)
 
 admin.site.register(UserRole)
 admin.site.register(KYCDocumentType)
 admin.site.register(ZrAdminUser)
-admin.site.register(ZrUser)
 admin.site.register(KYCDetail)
 admin.site.register(BankDetail)
 admin.site.register(OTPDetail)
