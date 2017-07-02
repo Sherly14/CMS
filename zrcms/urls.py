1d8e9f6a1c426aaefb312a2008dbf0abc59123af
Penkey Suresh <penkeysuresh@gmail.com>
2017-07-02 14:15:10 +0530

    adding base template

--- zrcms/urls.py
+++ zrcms/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^staff/login/$', auth_views.login, name='login'),
-    url(r'^staff/logout/$', auth_views.logout, name='logout'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
     url(r'^user/', include('zruser.urls')),
 ]
