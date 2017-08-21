fa4faaa85ef58756b695b1f44271b0665982e554
Penkey Suresh <penkeysuresh@gmail.com>
2017-08-21 21:27:20 +0530

    making comission table changes

--- zrcommission/models.py
+++ zrcommission/models.py
@@ -5,7 +5,7 @@ from decimal import Decimal
 
 from django.db import models
 
-from zrtransaction.models import Transaction
+from zrtransaction.models import Transaction, TransactionType, Vendor
 from zruser.models import ZrUser
 from zrutils.common.modelutils import RowInfo
 
@@ -14,18 +14,15 @@ from zrutils.common.modelutils import RowInfo
 
 class Commission(RowInfo):
 
-    transaction = models.ForeignKey(to=Transaction)
-    agent = models.ForeignKey(to=ZrUser, null=True, blank=True, related_name='agent_commission')
+    transaction = models.ForeignKey(to=Transaction, related_name='commissions')
     merchant = models.ForeignKey(to=ZrUser, null=True, blank=True, related_name='merchant_commission')
     distributor = models.ForeignKey(to=ZrUser, null=True, blank=True, related_name='distributor_commission')
-    agent_commission = models.DecimalField(max_digits=10, decimal_places=3, default=0.00)
     merchant_commission = models.DecimalField(max_digits=10, decimal_places=3, default=0.00)
     distributor_commission = models.DecimalField(max_digits=10, decimal_places=3, default=0.00)
     zrupee_commission = models.DecimalField(max_digits=10, decimal_places=3, default=0.00)
     government_tax = models.DecimalField(max_digits=10, decimal_places=3, default=0.00)
 
     def save(self, *args, **kwargs):
-        self.agent_commission = Decimal(self.agent_commission).quantize(Decimal("0.00"))
         self.distributor_commission = Decimal(self.distributor_commission).quantize(Decimal("0.00"))
         self.merchant_commission = Decimal(self.merchant_commission).quantize(Decimal("0.00"))
         self.zrupee_commission = Decimal(self.zrupee_commission).quantize(Decimal("0.00"))
@@ -38,3 +35,26 @@ class Commission(RowInfo):
 
     def __unicode__(self):
         return '%s - zrupee_commission com%s' % (self.transaction, self.zrupee_commission)
+
+
+class CommissionPercentage(RowInfo):
+
+    distributor = models.ForeignKey(to=ZrUser, related_name='commission_percentage')
+    transaction_type = models.ForeignKey(to=TransactionType)
+    transaction_vendor = models.ForeignKey(to=Vendor)
+    distributor_percentage = models.DecimalField(max_digits=3, decimal_places=3, default=0.00)
+    merchant_percentage = models.DecimalField(max_digits=3, decimal_places=3, default=0.00)
+    zrupee_percentage = models.DecimalField(max_digits=3, decimal_places=3, default=0.00)
+
+    def save(self, *args, **kwargs):
+        self.merchant_commission = Decimal(self.merchant_commission).quantize(Decimal("0.00"))
+        self.distributor_commission = Decimal(self.distributor_commission).quantize(Decimal("0.00"))
+        self.zrupee_percentage = Decimal(self.zrupee_percentage).quantize(Decimal("0.00"))
+
+        super(CommissionPercentage, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'CommissionPercentages'
+
+    def __unicode__(self):
+        return '%s - distributor_commission %s' % (self.distributor, self.distributor_commission)
