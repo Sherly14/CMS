2fc24282d32645fc5f13d38954e173c84c52ea9a
hitul <mistryhitul007@gmail.com>
2017-09-09 19:16:42 +0530

    Commission calculated

---
+++ zrcms/scripts/calculate_commissions.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import django
+import uuid
+import decimal
+
+
+cur_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(cur_dir, '..'))  # NOQA
+sys.path.append(os.path.join(cur_dir, '..', '..'))
+import settings  # NOQA
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')  # NOQA
+django.setup()  # NOQA
+
+from common_utils import transaction_utils
+transaction_utils.calculate_commission()
