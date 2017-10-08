34d374b07e65b356a962e34cf07b71c7363045f5
hitul <mistryhitul007@gmail.com>
2017-10-08 23:15:04 +0530

    Payment request count fixed

--- zrpayment/templatetags/payment_request_tags.py
+++ zrpayment/templatetags/payment_request_tags.py
@@ -11,11 +11,12 @@ def payment_req_cnt(request):
     pr_cnt = 0
     if is_user_superuser(request):
         pr_cnt = PaymentRequest.objects.filter(
-            status="0"
+            status=0
         ).count()
     elif request.user.zr_admin_user.role.name in ['DISTRIBUTOR', 'SUBDISTRIBUTOR']:
         pr_cnt = PaymentRequest.objects.filter(
-            to_user=request.user.zr_admin_user.zr_user
+            to_user=request.user.zr_admin_user.zr_user,
+            status=0
         ).exclude(from_user=request.user.zr_admin_user.zr_user).count()
 
     return pr_cnt
