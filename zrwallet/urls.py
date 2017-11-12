da0f436e322bd80e0a79afbdc5ef3a41c040ec05
hitul <mistryhitul007@gmail.com>
2017-11-12 17:17:09 +0530

    Passbook download csv using mail in background bug fix in payments reports: filter not reflecting

--- zrwallet/urls.py
+++ zrwallet/urls.py
@@ -4,5 +4,5 @@ from . import views as zr_wallet_views
 
 urlpatterns = [
     url(r'^passbook/$', zr_wallet_views.PaymentListView.as_view(), name='passbook-list'),
-    # url(r'^passbook-csv/$', zr_wallet_views.payments_csv_download, name='passbook-csv'),
+    url(r'^passbook-csv/$', zr_wallet_views.payments_csv_download, name='passbook-csv'),
 ]
