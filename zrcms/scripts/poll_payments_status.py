8eee763f0625c5f7b7e87ba69f1a20c67d015009
hitul <mistryhitul007@gmail.com>
2017-11-17 02:54:31 +0530

    Modified cron job logic

--- zrcms/scripts/poll_payments_status.py
+++ zrcms/scripts/poll_payments_status.py
@@ -3,16 +3,23 @@ from zrpayment.models import Payments
 
 
 def poll_payments_for_lastest_status():
-    payments = Payments.objects.all()
+    payments = Payments.objects.filter(
+        status=Payments.status[0][0]
+    )
     # TODO: define status field in Payments model
     print 'polling payments status for queryset ', payments
 
     for payment_obj in payments:
-        response = get_payment_status(payment_obj.txn_id)
+        response = get_payment_status(payment_obj.vendor_txn_id)
         if response:
-            try:
-                payment_obj.status = response['status']
-                payment_obj.transaction_response_json = response
-                payment_obj.save()
-            except:
-                pass
+            if response['status'] == "CONFIRMED":
+                payment_obj.status = Payments.status[1][0]
+                payment_obj.transaction_response_json["%s_RESPONSE" % response['status']] = response
+            elif response['status'] == "FAILED":
+                payment_obj.status = Payments.status[2][0]
+                payment_obj.transaction_response_json["%s_RESPONSE" % response['status']] = response
+            elif response['status'] == "EXPIRED":
+                payment_obj.status = Payments.status[5][0]
+                payment_obj.transaction_response_json["%s_RESPONSE" % response['status']] = response
+
+            payment_obj.save()
