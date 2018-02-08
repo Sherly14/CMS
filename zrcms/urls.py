e6779d6014abfb76842e93e4ca6e278224ef5cda
pankaj <pankaj@zrupee.com>
2018-02-08 20:01:24 +0530

    distributor listview filter

--- zrcms/urls.py
+++ zrcms/urls.py
@@ -35,6 +35,6 @@ urlpatterns = [
     url(r'^password_reset/done/$', auth_views.password_reset_done, {'template_name': 'password_reset_done.html'}, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth_views.password_reset_confirm,{'template_name': 'password_reset_confirm.html'}, name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.password_reset_complete,{'template_name': 'password_reset_complete.html'}, name='password_reset_complete'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, {'template_name': 'password_reset_complete.html'}, name='password_reset_complete'),
 
 ]
