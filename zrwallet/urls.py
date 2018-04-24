d102b33bccef921c74a9fe96355baaae435fdf96
pankaj <pankaj@zrupee.com>
2018-04-24 15:28:07 +0530

    passbook

--- zrwallet/urls.py
+++ zrwallet/urls.py
@@ -4,6 +4,6 @@ from . import views as zr_wallet_views
 
 urlpatterns = [
     url(r'^passbook/$', zr_wallet_views.PassbookListView.as_view(), name='passbook-list'),
-    url(r'^passbook-csv/$', zr_wallet_views.payments_csv_download, name='passbook-csv'),
+    url(r'^passbook-csv/$', zr_wallet_views.get_passbook_report_csv, name='passbook-csv'),
     url(r'^update_transaction/$', zr_wallet_views.set_closing_balance, name='closing-balance'),
 ]
--- zrwallet/views.py
+++ zrwallet/views.py
@@ -2,28 +2,47 @@
 from __future__ import unicode_literals
 
 import csv
-import datetime
-import random
-import string
+from urllib import urlencode
 
 from django.conf import settings
 from django.core.paginator import Paginator, PageNotAnInteger
 from django.db.models import Q
 from django.db import transaction
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse, HttpResponse, Http404
 from django.views.generic.list import ListView
+from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
+
 
 from common_utils.date_utils import last_week_range, last_month
 from common_utils.user_utils import is_user_superuser
 from zrwallet.models import Wallet, WalletTransactions
 from zruser.models import ZrUser
 from zrmapping import models as zrmappings_models
+from zruser.mapping import SUBDISTRIBUTOR, DISTRIBUTOR, MERCHANT
+from common_utils.transaction_utils import get_merchant_id_list_from_distributor
 
 
 def get_transaction_qs(request):
-    queryset = WalletTransactions.objects.all().order_by('id')
+    # queryset = WalletTransactions.objects.all().order_by('id')
+    queryset = None
+    if is_user_superuser(request):
+        queryset = WalletTransactions.objects.all()
+
+    elif request.user.zr_admin_user.role.name == DISTRIBUTOR:
+        dist_subd = list(zrmappings_models.DistributorSubDistributor.objects.filter(
+            distributor=request.user.zr_admin_user.zr_user).values_list('sub_distributor_id', flat=True))
+        queryset = WalletTransactions.objects.filter(
+            wallet_id__in=get_merchant_id_list_from_distributor(request.user.zr_admin_user.zr_user) +
+            dist_subd + list(zrmappings_models.SubDistributorMerchant.objects.filter(
+                sub_distributor__in=dist_subd).values_list('merchant_id', flat=True)))
+
+    elif request.user.zr_admin_user.role.name == SUBDISTRIBUTOR:
+        queryset = WalletTransactions.objects.filter(
+            wallet_id__in=list(zrmappings_models.SubDistributorMerchant.objects.filter(
+                sub_distributor=request.user.zr_admin_user.zr_user).values_list(
+                'merchant_id', flat=True)))
+
     q = request.GET.get('q')
-    filter = request.GET.get('filter')
     if q:
         query_filter = Q(wallet__merchant__first_name__icontains=q) | Q(wallet__merchant__last_name__icontains=q) | \
                        Q(wallet__merchant__mobile_no__contains=q)
@@ -34,25 +53,12 @@ def get_transaction_qs(request):
 
     start_date = request.GET.get('startDate')
     end_date = request.GET.get('endDate')
-    distributor_id = request.GET.get('distributor-id')
-    if start_date!=None and end_date!=None:
-        queryset=queryset.filter(at_created__range=(start_date, end_date))
+    user_id = request.GET.get('user_id')
+    if start_date is not None and end_date is not None:
+        queryset = queryset.filter(at_created__range=(start_date, end_date))
 
-    if distributor_id!=None and int(distributor_id) > 0:
-        queryset=queryset.filter(wallet_id=distributor_id)
-
-    # distributor_id = request.GET.get('distributor-id')
-    #
-    # if distributor_id!=None and int(distributor_id) > 0:
-    #     queryset = queryset.filter(id=distributor_id)
-    #
-    #
-    # if filter == 'Today':
-    #     queryset = queryset.filter(at_created__gte=datetime.datetime.now().date())
-    # elif filter == 'Last-Week':
-    #     queryset = queryset.filter(at_created__range=last_week_range())
-    # elif filter == 'Last-Month':
-    #     queryset = queryset.filter(at_created__range=last_month())
+    if user_id is not None and int(user_id) > 0:
+        queryset = queryset.filter(wallet_id=user_id)
 
     return queryset
 
@@ -64,135 +70,100 @@ class PassbookListView(ListView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(PassbookListView, self).get_context_data()
-        passbook_list = WalletTransactions.objects.all()
-        user_list = WalletTransactions.objects.all().distinct('wallet')
+        user_list = None
         filter_by = self.request.GET.get('filter', 'all')
         q = self.request.GET.get('q')
-        context['passbook_list'] = passbook_list
-        queryset = WalletTransactions.objects.all()
-        page = self.request.GET.get('page', 1)
+        queryset = self.get_queryset()
+        pg_no = self.request.GET.get('page_no', 1)
         start_date = self.request.GET.get('startDate')
         end_date = self.request.GET.get('endDate')
-        distributor_id = self.request.GET.get('distributor-id')
-        context = super(PassbookListView, self).get_context_data(**kwargs)
+        user_id = self.request.GET.get('user_id')
 
+        if is_user_superuser(self.request):
+            user_list = WalletTransactions.objects.all().distinct('wallet_id')
+
+        elif self.request.user.zr_admin_user.role.name == DISTRIBUTOR:
+            dist_subd = list(zrmappings_models.DistributorSubDistributor.objects.filter(
+                distributor=self.request.user.zr_admin_user.zr_user).values_list('sub_distributor_id', flat=True))
+            user_list = WalletTransactions.objects.filter(
+                wallet_id__in=get_merchant_id_list_from_distributor(self.request.user.zr_admin_user.zr_user) +
+                dist_subd + list(zrmappings_models.SubDistributorMerchant.objects.filter(
+                    sub_distributor__in=dist_subd).values_list('merchant_id', flat=True))).distinct('wallet_id')
+
+        elif self.request.user.zr_admin_user.role.name == SUBDISTRIBUTOR:
+            user_list = WalletTransactions.objects.filter(
+                wallet_id__in=list(zrmappings_models.SubDistributorMerchant.objects.filter(
+                    sub_distributor=self.request.user.zr_admin_user.zr_user).values_list(
+                    'merchant_id', flat=True))).distinct('wallet_id')
+        if q:
+            context['q'] = q
         if start_date:
             context['startDate'] = start_date
-
         if end_date:
             context['endDate'] = end_date
-
-        if distributor_id:
-            context['distributor_id'] = int(distributor_id)
+        if user_id:
+            context['user_id'] = int(user_id)
         if user_list:
             context['user_list'] = user_list
 
-        #queryset = self.get_queryset()
-        paginator = Paginator(queryset, self.paginate_by)
-
+        p = Paginator(queryset, self.paginate_by)
         try:
-            queryset = paginator.page(page)
-        except PageNotAnInteger:
-            queryset = paginator.page(1)
+            page = p.page(pg_no)
+        except EmptyPage:
+            raise Http404
 
-        from django.core.urlresolvers import reverse
+        context['queryset'] = page.object_list
+        context['url_name'] = "passbook-list"
 
-        context['main_url'] = reverse('wallet:passbook-list')
-        context['page_obj'] = queryset
-        context['filter_by'] = filter_by
-        context['q'] = q
+        query_string = {}
+        if q:
+            query_string['q'] = q
+        if user_id:
+            query_string['user_id'] = int(user_id)
+        if start_date:
+            query_string['startDate'] = start_date
+        if end_date:
+            query_string['endDate'] = end_date
+
+        print urlencode(query_string)
+
+        if page.has_next():
+            query_string['page_no'] = page.next_page_number()
+            context['next_page_qs'] = urlencode(query_string)
+            context['has_next_page'] = page.has_next()
+        if page.has_previous():
+            query_string['page_no'] = page.previous_page_number()
+            context['prev_page_qs'] = urlencode(query_string)
+            context['has_prev_page'] = page.has_previous()
 
         return context
 
-# ------------******------------
-#     context = super(DistributorListView, self).get_context_data()
-#     queryset = self.get_queryset()
-#     distributor_list = ZrUser.objects.filter(role__name=DISTRIBUTOR)
-#     q = self.request.GET.get('q')
-#     filter = self.request.GET.get('filter')
-#     pg_no = self.request.GET.get('page_no', 1)
-#     start_date = self.request.GET.get('startDate')
-#     end_date = self.request.GET.get('endDate')
-#     distributor_id = self.request.GET.get('distributor-id')
-#
-#     if activate:
-#         zruser = ZrUser.objects.filter(id=activate).last()
-#         if not zruser:
-#             raise Http404
-#
-#         zruser.is_active = True
-#         zrmappings_models.DistributorMerchant.objects.filter(
-#             distributor=zruser
-#         ).update(
-#             is_attached_to_admin=False
-#         )
-#         dj_user = zruser.zr_user
-#         dj_user.is_active = True
-#         dj_user.save(update_fields=['is_active'])
-#         zruser.save(update_fields=['is_active'])
-#
-#     if disable:
-#         zruser = ZrUser.objects.filter(id=disable).last()
-#         if not zruser:
-#             raise Http404
-#
-#         zruser.is_active = False
-#         zrmappings_models.DistributorMerchant.objects.filter(
-#             distributor=zruser
-#         ).update(
-#             is_attached_to_admin=True
-#         )
-#         dj_user = zruser.zr_user
-#         dj_user.is_active = False
-#         dj_user.save(update_fields=['is_active'])
-#         zruser.save(update_fields=['is_active'])
-#
-#     if q:
-#         context['q'] = q
-#
-#     if filter:
-#         context['filter_by'] = filter
-#
-#     if start_date:
-#         context['startDate'] = start_date
-#
-#     if end_date:
-#         context['endDate'] = end_date
-#
-#     if distributor_id:
-#         context['distributor_id'] = int(distributor_id)
-#
-#     context['distributor_list'] = distributor_list
-#     context['queryset'] = queryset
-#     p = Paginator(context['queryset'], self.paginate_by)
-#     try:
-#         page = p.page(pg_no)
-#     except EmptyPage:
-#         raise Http404
-#
-#     context['queryset'] = page.object_list
-#     query_string = {}
-#     if q:
-#         query_string['q'] = q
-#
-#     if filter:
-#         query_string['filter'] = filter
-#
-#     if page.has_next():
-#         query_string['page_no'] = page.next_page_number()
-#         context['next_page_qs'] = urlencode(query_string)
-#         context['has_next_page'] = page.has_next()
-#     if page.has_previous():
-#         query_string['page_no'] = page.previous_page_number()
-#         context['prev_page_qs'] = urlencode(query_string)
-#         context['has_prev_page'] = page.has_previous()
-#
-#     return context
-#
-# --------****-----------
-
     def get_queryset(self):
         return get_transaction_qs(self.request)
+
+
+def get_passbook_report_csv(request):
+    passbook_qs = get_transaction_qs(request)
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="passbook.csv"'
+    writer = csv.writer(response)
+    writer.writerow(['Date', 'User name', 'Mobile No', 'Role', 'Transaction ID', 'Transaction type',
+                     'Payment ID', 'Transaction Value', 'DMT Balance', 'Non DMT Balance'])
+    for passbook in passbook_qs:
+        writer.writerow([
+            passbook.at_created,
+            passbook.wallet.merchant.full_name,
+            passbook.wallet.merchant.mobile_no,
+            passbook.wallet.merchant.role.name,
+            passbook.transaction_id,
+            passbook.transaction.type if passbook.transaction is not None else '',
+            passbook.payment_request_id,
+            passbook.dmt_balance if passbook.dmt_balance != 0 else passbook.non_dmt_balance,
+            passbook.dmt_closing_balance,
+            passbook.non_dmt_closing_balance
+        ])
+
+    return response
 
 
 def payments_csv_download(request):
