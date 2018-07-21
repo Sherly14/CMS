64fc719836738dd6f9a36f523ce9b0459e1fbc48
pkyzr <pankaj@zrupee.com>
2018-07-21 17:20:36 +0530

    initiator_id

--- zrcms/env_vars.py
+++ zrcms/env_vars.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.getenv('APP_SECRET_KEY', LOCAL_SECRET_KEY)
 
 # EKO credentials
 # Trying to get the credentials from environment Variables, falling back to sandbox environment
-EKO_INITIATOR_ID = os.getenv('EKO_INITIATOR_ID', '9910028267')
+EKO_INITIATOR_ID = os.getenv('EKO_INITIATOR_ID', '7506988914')
 EKO_DEVELOPER_KEY = os.getenv('EKO_DEVELOPER_KEY', 'becbbce45f79c6f5109f848acd540567')
 EKO_TRANSACTION_ENQUIRY_URL = os.getenv('EKO_TRANSACTION_ENQUIRY_URL',
                                         'https://staging.eko.co.in:25004/ekoapi/v1/transactions/')
