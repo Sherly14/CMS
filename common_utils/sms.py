7e432568ff9767649fb544097481b17d0a985afd
pankaj <pankaj@zrupee.com>
2018-10-04 17:18:24 +0530

    top up sms

--- common_utils/sms.py
+++ common_utils/sms.py
@@ -18,30 +18,35 @@ BHASHSMS_SERVICE = {
 
 
 def send_sms(sender='ZRUPEE', phone='', text='', priority='', stype='normal'):
+    if phone == '' or text == '' or priority == '':
+        print 'Incomplete parameters'
+        return
     sms_url = BHASHSMS_BASE_URL + BHASHSMS_SERVICE['sendmsg'] + '.php?' + 'user=' + BHASHSMS_USERNAME \
                      + '&pass=' + BHASHSMS_PASSWORD \
                      + '&sender=' + sender + '&phone=' + phone \
                      + '&text=' + text + '&priority=' + priority + '&stype=' + stype
 
-    print 'single_sms_url ', sms_url
-    sms_req = requests.get(url=sms_url)
-    print 'send_sms' + sms_req.content
+    try:
+        sms_req = requests.get(url=sms_url)
+        print 'sms_req response' + sms_req.content
+    except requests.RequestException as e:
+        print 'error - ', e
 
 
 def wallet_sms(payment_request, zr_wallet):
-    print 'payment_request', payment_request
-    phone = payment_request.from_user.mobile_no # prod
-    # phone = '9987642331'  # dev
-    print 'phone', phone, ' first_name ', payment_request.from_user.first_name
+    if not payment_request or not zr_wallet:
+        print 'payment_request or zr_wallet is None'
+        return
 
+    phone = payment_request.from_user.mobile_no  # prod
     accounting_entry = "credited" if payment_request.amount >= 0 else "debited"
     text = 'Hi ' + str(payment_request.from_user.first_name).title() + ' ' + \
            str(payment_request.from_user.last_name).title() + \
            ', your Zrupee Wallet is ' + accounting_entry + ' with Rs. ' + \
-           abs(payment_request.amount) + '. Your current Wallet Balance is Rs. ' + \
-           zr_wallet.get_total_balance()
+           str(abs(payment_request.amount)) + '. Your current Wallet Balance is Rs. ' + \
+           str(zr_wallet.get_total_balance())
     try:
         send_sms(sender='ZRUPEE', phone=phone,
                  text=text, priority=BHASHSMS_PRIORITY['ndnd'], stype='normal')
     except Exception as e:
-        print('error - ', e)
+        print 'error - ', e
