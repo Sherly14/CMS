b47ddcc6d4cee65dd424713f12e128944ac232f3
pankaj <pankaj@zrupee.com>
2018-11-16 22:40:09 +0530

    prod, locale

--- common_utils/sms.py
+++ common_utils/sms.py
@@ -1,4 +1,5 @@
 import requests
+import locale
 
 from zrcms.env_vars import BHASHSMS_BASE_URL, BHASHSMS_USERNAME, BHASHSMS_PASSWORD, ENVIRONMENT
 
@@ -31,6 +32,8 @@ def send_sms(sender='ZRUPEE', phone='', text='', priority='', stype='normal'):
             print 'sms_req response' + sms_req.content
         else:
             print "ENVIRONMENT not PRODUCTION"
+            print "sms text - \n", text
+            pass
     except requests.RequestException as e:
         print 'error - ', e
 
@@ -40,15 +43,20 @@ def wallet(wallet_transaction):
         print 'SMS: wallet_transaction is None'
         return
 
+    locale.setlocale(locale.LC_NUMERIC, '')
+
     phone = wallet_transaction.wallet.merchant.mobile_no  # prod
-    amount = wallet_transaction.dmt_balance + wallet_transaction.non_dmt_balance
-    balance = wallet_transaction.dmt_closing_balance + wallet_transaction.non_dmt_closing_balance
-    accounting_entry = "credited" if amount >= 0 else "debited"
+
+    accounting_entry = "is credited with" if (wallet_transaction.dmt_balance + wallet_transaction.non_dmt_balance) >= 0 else "has been debited for"
+
+    amount = locale.format("%.2f", abs(wallet_transaction.dmt_balance + wallet_transaction.non_dmt_balance), grouping=True)
+    balance = locale.format("%.2f", wallet_transaction.dmt_closing_balance + wallet_transaction.non_dmt_closing_balance, grouping=True)
+
     text = 'Hi ' + str(wallet_transaction.wallet.merchant.first_name).title() + ' ' + \
            str(wallet_transaction.wallet.merchant.last_name).title() + \
-           ', your Zrupee Wallet is ' + accounting_entry + ' with Rs. ' + \
-           str(abs(amount)) + '. Wallet Balance - Rs. ' + \
-           str(balance) + '\n. Keep up the Balance. Zrupee.'
+           ', your Zrupee Wallet ' + accounting_entry + ' INR ' + \
+           str(amount) + '.\nWallet Balance - INR ' + \
+           str(balance) + '\nKeep up the Balance!'
     try:
         send_sms(sender='ZRUPEE', phone=str(phone),
                  text=text, priority=BHASHSMS_PRIORITY['ndnd'], stype='normal')
