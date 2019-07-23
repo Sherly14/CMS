026f84cc7833876d479078a56936dc31969a1d41
pkyzr <pankaj@zrupee.com>
2019-07-23 17:48:44 +0530

    poll tran status try catch

--- zrcms/scripts/poll_transaction_status.py
+++ zrcms/scripts/poll_transaction_status.py
@@ -25,6 +25,7 @@ from zrtransaction.utils.constants import POLL_EXCLUDED_STATUS, TX_STATUS_AWAITE
     TRANSACTION_STATUS_REFUNDED
 from zrwallet.models import WalletTransactions
 from django.db import transaction as dj_transaction
+import traceback
 
 
 def poll_transaction_status_for_refund():
@@ -36,98 +37,102 @@ def poll_transaction_status_for_refund():
             print 'polling transaction status for queryset ', transactions
 
             for transaction in transactions:
-                sleep(1)
-                txn_id = transaction.vendor_txn_id
-                headers = {'developer_key': EKO_DEVELOPER_KEY, 'cache-control': "no-cache"}
-                url = EKO_TRANSACTION_ENQUIRY_URL + txn_id + '?initiator_id=' + EKO_INITIATOR_ID
-                print 'making eko api request ', 'URL: ', url, 'headers: ', headers
-                response_data = requests.get(url, headers=headers).json()
-                print response_data, response_data['status']
+                try:
+                    sleep(1)
+                    txn_id = transaction.vendor_txn_id
+                    headers = {'developer_key': EKO_DEVELOPER_KEY, 'cache-control': "no-cache"}
+                    url = EKO_TRANSACTION_ENQUIRY_URL + txn_id + '?initiator_id=' + EKO_INITIATOR_ID
+                    print 'making eko api request ', 'URL: ', url, 'headers: ', headers
+                    response_data = requests.get(url, headers=headers).json()
+                    print response_data, response_data['status']
 
-                # refer to eko documentation for tx_status responses http://developers.eko.co.in/docs/index.html
-                if response_data['status'] == 0:
-                    if int(response_data['data']['tx_status']) == TX_STATUS_SUCCESS:
-                        print ' transaction ', transaction.vendor_txn_id
-                        transaction.status = TRANSACTION_STATUS_SUCCESS
-                        transaction.save()
+                    # refer to eko documentation for tx_status responses http://developers.eko.co.in/docs/index.html
+                    if response_data['status'] == 0:
+                        if int(response_data['data']['tx_status']) == TX_STATUS_SUCCESS:
+                            print ' transaction ', transaction.vendor_txn_id
+                            transaction.status = TRANSACTION_STATUS_SUCCESS
+                            transaction.save()
 
-                        if transaction.transaction_response_json is None:
-                            transaction.transaction_response_json = dict(poll_success_response=json.dumps(response_data))
-                        else:
-                            transaction.transaction_response_json['poll_success_response'] = json.dumps(response_data)
-                        transaction.save()
-
-                    elif int(response_data['data']['tx_status']) == TX_STATUS_FAIL:
-                        # transaction.status = TRANSACTION_STATUS_FAILURE
-                        transaction.save()
-
-                        if transaction.transaction_response_json is None:
-                            transaction.transaction_response_json = dict(poll_failure_response=json.dumps(response_data))
-                        else:
-                            transaction.transaction_response_json['poll_failure_response'] = json.dumps(response_data)
-                        transaction.save()
-
-                        #     send a mail to the zrupee tech
-                        email_utils.send_email(
-                            'Error in EKO refund polling!',
-                            'tech@zrupee.com',
-                            'payment_status_error',
-                            {
-                                'url': url,
-                                'response_json': json.dumps(response_data)
-                            },
-                            is_html=True
-                        )
-
-                    elif int(response_data['data']['tx_status']) == TX_STATUS_AWAITED:
-                        pass
-
-                    elif int(response_data['data']['tx_status']) == TX_STATUS_REFUND_PENDING:
-
-                        if transaction.status != TRANSACTION_STATUS_REFUND_PENDING:
                             if transaction.transaction_response_json is None:
-                                transaction.transaction_response_json = dict(
-                                    poll_refund_pending_response=json.dumps(response_data))
+                                transaction.transaction_response_json = dict(poll_success_response=json.dumps(response_data))
                             else:
-                                transaction.transaction_response_json['poll_refund_pending_response'] = json.dumps(response_data)
+                                transaction.transaction_response_json['poll_success_response'] = json.dumps(response_data)
+                            transaction.save()
 
-                        transaction.status = TRANSACTION_STATUS_REFUND_PENDING
-                        transaction.save()
+                        elif int(response_data['data']['tx_status']) == TX_STATUS_FAIL:
+                            # transaction.status = TRANSACTION_STATUS_FAILURE
+                            transaction.save()
 
-                    elif int(response_data['data']['tx_status']) == TX_STATUS_REFUNDED and \
-                            transaction.status != 'R':
-                        merchant_wallet = transaction.user.wallet
-                        refund_amount = transaction.amount + transaction.additional_charges
+                            if transaction.transaction_response_json is None:
+                                transaction.transaction_response_json = dict(poll_failure_response=json.dumps(response_data))
+                            else:
+                                transaction.transaction_response_json['poll_failure_response'] = json.dumps(response_data)
+                            transaction.save()
 
-                        wallet_log = WalletTransactions()
-                        wallet_log.wallet = merchant_wallet
-                        wallet_log.transaction = transaction
+                            #     send a mail to the zrupee tech
+                            email_utils.send_email(
+                                'Error in EKO refund polling!',
+                                'tech@zrupee.com',
+                                'payment_status_error',
+                                {
+                                    'url': url,
+                                    'response_json': json.dumps(response_data)
+                                },
+                                is_html=True
+                            )
 
-                        if transaction.type.name.upper() == 'DMT':
+                        elif int(response_data['data']['tx_status']) == TX_STATUS_AWAITED:
+                            pass
 
-                            merchant_wallet.dmt_balance += refund_amount
-                            merchant_wallet.save()
-                            wallet_log.dmt_balance = refund_amount
-                            wallet_log.dmt_closing_balance = merchant_wallet.dmt_balance
-                        else:
-                            merchant_wallet.non_dmt_balance += refund_amount
-                            merchant_wallet.save()
-                            wallet_log.non_dmt_balance = refund_amount
-                            wallet_log.non_dmt_closing_balance = wallet_log.non_dmt_balance
-                        wallet_log.save()
-                        transaction.status = TRANSACTION_STATUS_REFUNDED
-                        transaction.save()
+                        elif int(response_data['data']['tx_status']) == TX_STATUS_REFUND_PENDING:
 
-                        if transaction.transaction_response_json is None:
-                            transaction.transaction_response_json = dict(poll_refunded_response=json.dumps(response_data))
-                        else:
-                            transaction.transaction_response_json['poll_refunded_response'] = json.dumps(response_data)
-                        transaction.save()
+                            if transaction.status != TRANSACTION_STATUS_REFUND_PENDING:
+                                if transaction.transaction_response_json is None:
+                                    transaction.transaction_response_json = dict(
+                                        poll_refund_pending_response=json.dumps(response_data))
+                                else:
+                                    transaction.transaction_response_json['poll_refund_pending_response'] = json.dumps(response_data)
 
+                            transaction.status = TRANSACTION_STATUS_REFUND_PENDING
+                            transaction.save()
+
+                        elif int(response_data['data']['tx_status']) == TX_STATUS_REFUNDED and \
+                                transaction.status != 'R':
+                            merchant_wallet = transaction.user.wallet
+                            refund_amount = transaction.amount + transaction.additional_charges
+
+                            wallet_log = WalletTransactions()
+                            wallet_log.wallet = merchant_wallet
+                            wallet_log.transaction = transaction
+
+                            if transaction.type.name.upper() == 'DMT':
+
+                                merchant_wallet.dmt_balance += refund_amount
+                                merchant_wallet.save()
+                                wallet_log.dmt_balance = refund_amount
+                                wallet_log.dmt_closing_balance = merchant_wallet.dmt_balance
+                            else:
+                                merchant_wallet.non_dmt_balance += refund_amount
+                                merchant_wallet.save()
+                                wallet_log.non_dmt_balance = refund_amount
+                                wallet_log.non_dmt_closing_balance = wallet_log.non_dmt_balance
+                            wallet_log.save()
+                            transaction.status = TRANSACTION_STATUS_REFUNDED
+                            transaction.save()
+
+                            if transaction.transaction_response_json is None:
+                                transaction.transaction_response_json = dict(poll_refunded_response=json.dumps(response_data))
+                            else:
+                                transaction.transaction_response_json['poll_refunded_response'] = json.dumps(response_data)
+                            transaction.save()
+                except Exception as e:
+                    print(transaction, transaction.pk)
+                    print(traceback.format_exc())
+                    print '1 error -> ', e
     except Exception as e:
         # print(transaction, transaction.pk, transaction.is_commission_created)
-
-        print 'error- ', e
+        print(traceback.format_exc())
+        print '2 error -> ', e
 
 
 poll_transaction_status_for_refund()
