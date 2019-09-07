ba7d97e61873541c2ccc8c4a714dc83d867bed81
pankaj <pankaj@zrupee.com>
2019-09-07 17:50:24 +0530

    feeback model

--- feedback/models.py
+++ feedback/models.py
@@ -3,10 +3,12 @@ from __future__ import unicode_literals
 from django.contrib.postgres.fields import JSONField
 from zrutils.common.modelutils import RowInfo, get_slugify_value
 from django.db import models
+from zruser.models import ZrUser
 
 
 class Feedback(RowInfo):
     feedback_json = JSONField(null=True, blank=True)
+    user = models.ForeignKey(ZrUser, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.feedback_json
